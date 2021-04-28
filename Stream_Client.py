34ff441dc39e3e80e21eb5babe74a3f9332adc12
hyfung <hyfung@link.cuhk.edu.hk>
2021-04-28 08:34:07 +0800

    Added buffering to client too

--- Stream_Client.py
+++ Stream_Client.py
@@ -1,21 +1,32 @@
 import cv2, numpy as np
 import os, sys
 import socket
-import threading
+import threading, queue
 import time
 
-def main():
+q = queue.Queue()
+
+def captureThread():
     cap = cv2.VideoCapture(0)
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
-    count = 0
     while True:
         ret, img = cap.read()
         img = cv2.resize(img, (320,240))
         ret, img_encode = cv2.imencode('.jpg', img)
-        client.sendto(img_encode.tobytes(), ("127.0.0.1", 9999))
+        q.put(img_encode.tobytes())
+        time.sleep(0.1)        
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    tCapture = threading.Thread(target=captureThread)
+    tCapture.start()
+
+    count = 0
+    while True:
+        client.sendto(q.get(), ("127.0.0.1", 9999))
         time.sleep(0.1)
-        print(count)
+        print(f'[Send] {count}')
         count += 1
 
 if __name__ == '__main__':
