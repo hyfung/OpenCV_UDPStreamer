5b37f3e347ba2f6c28ac1a04093d31aa92aa2dc6
Jerry Fung <hyfung@link.cuhk.edu.hk>
2021-07-11 17:41:10 +0800

    Added SIGINT handler to stop the threads

--- Stream_Client.py
+++ Stream_Client.py
@@ -3,27 +3,37 @@ import os, sys
 import socket
 import threading, queue
 import time
+import signal
 
 q = queue.Queue()
+run = True
+
+def sigint_handler(signum, frame):
+    # This function is called when Ctrl-C or SIGINT is received
+    # Exit gracefully
+    global run
+    run = False
 
 def captureThread():
     cap = cv2.VideoCapture(0)
     
-    while True:
+    while run:
         ret, img = cap.read()
         img = cv2.resize(img, (320,240))
         ret, img_encode = cv2.imencode('.jpg', img)
         q.put(img_encode.tobytes())
-        time.sleep(0.1)        
+        cv2.imshow("Transmitting Feed", img)
+        cv2.waitKey(100)
 
 def main():
+    signal.signal(signal.SIGINT, sigint_handler)
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     tCapture = threading.Thread(target=captureThread)
     tCapture.start()
 
     count = 0
-    while True:
+    while run:
         client.sendto(q.get(), ("127.0.0.1", 9999))
         time.sleep(0.1)
         print(f'[Send] {count}')
--- Stream_Server.py
+++ Stream_Server.py
@@ -3,18 +3,27 @@ import os, sys
 import socket
 import threading, queue
 import time
+import signal
 
 q = queue.Queue()
+run = True
+
+def sigint_handler(signum, frame):
+    # This function is called when Ctrl-C or SIGINT is received
+    # Exit gracefully
+    global run
+    run = False
 
 def displayThread():
-    while True:
+    while run:
         data = q.get()
         tmp = np.asarray(bytearray(data), dtype='uint8')
         img = cv2.imdecode(tmp, cv2.IMREAD_COLOR)
-        cv2.imshow("Window", img)
+        cv2.imshow("Receiving Feed", img)
         cv2.waitKey(100)
 
 def main():
+    signal.signal(signal.SIGINT, sigint_handler)
     img = None
     bind_ip = "0.0.0.0"
     bind_port = 9999
@@ -27,10 +36,9 @@ def main():
     tDisplay.start()
 
     count = 0
-    while True:
+    while run:
         data, addr = server.recvfrom(65535)
         q.put(data)
-        # print(f"[Recv]{addr}: {data}")
         print(f"[Recv]{addr}: {count}")
         count += 1
 
