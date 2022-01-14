d6f1b59c3d5dcb0d38732c8c6da2dff174b10222
Jerry Fung <hyfung@link.cuhk.edu.hk>
2022-01-14 12:44:30 -0500

    Added videos as test input

--- test_Stream_Client.py
+++ test_Stream_Client.py
@@ -1,18 +1,21 @@
 from Stream_Client import *
-
+import pytest
 import time
 
 def test_captureThread():
+
     tCapture = threading.Thread(target=captureThread, args=("test.mp4",))
     tCapture.start()
 
     # Wait for 5 seconds
     time.sleep(5)
     run = False
-    # tCapture.join(1)
 
 def test_captureThread_shouldFail():
-    pass
+    # This should fail because no video input
+    with pytest.raises(Exception):
+        captureThread("test2.mp4")
+
 
 def test_sigint_handler():
     assert sigint_handler(signal.SIGINT, None) == True
